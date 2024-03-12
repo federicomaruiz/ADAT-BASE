3a0828e9aee921e01c89d2606d0ab4156fea5fd2
FedeRuiz <federicomaruiz@gmail.com>
2024-03-12 13:44:34 +0100

    b

--- src/main/java/com/utad/Probando/repository/UserRepository.java
+++ src/main/java/com/utad/Probando/repository/UserRepository.java
@@ -4,8 +4,7 @@ import com.utad.Probando.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Anotación para indicar que esta interfaz es un repositorio de Spring
 @Repository
-// Extiende JpaRepository para operaciones CRUD básicas en la base de datos
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
